a7e9981209059f70b6bedf22b429f01eafe9bf72
Paris Chatz <parischatz94@gmail.com>
2021-09-14 20:40:04 +0300

    request order node class installed in waitbot
    The request order node and specifically the req_order class is now able to be called from other packages.

--- waitbot/src/waitbot/request_order_node.py
+++ waitbot/src/waitbot/request_order_node.py
@@ -9,68 +9,65 @@ import rospy
 from std_msgs.msg import String
 
 
-
 class req_order():
     def __init__(self):
-
+        # TODO put all correct items
         self.items = ['cocacola',
-                'sprite',
-                'fanta']
-                # ,
-                # 'estathe',
-                # 'beer',
-                # 'pringles1',
-                # 'pringles2',
-                # 'bifrutas_tropical_can',
-                # 'biscuits_pack',
-                # 'cup_glass_new',
-                # 'cup_paper',
-                # 'peroni',
-                # 'plastic_cup',
-                # 'tea_box'
-                # ]
+                      'sprite',
+                      'fanta']
+        # ,
+        # 'estathe',
+        # 'beer',
+        # 'pringles1',
+        # 'pringles2',
+        # 'bifrutas_tropical_can',
+        # 'biscuits_pack',
+        # 'cup_glass_new',
+        # 'cup_paper',
+        # 'peroni',
+        # 'plastic_cup',
+        # 'tea_box'
+        # ]
 
         self.banner = """
         =================================================
-        _       __      _ __            ____        __ 
-        | |     / /___ _(_) /____  _____/ __ )____  / /_
-        | | /| / / __ `/ / __/ _ \/ ___/ __  / __ \/ __/
-        | |/ |/ / /_/ / / /_/  __/ /  / /_/ / /_/ / /_  
-        |__/|__/\__,_/_/\__/\___/_/  /_____/\____/\__/  
-                                                        
+        
+         _       __      _ __  __          __ 
+        | |     / /___ _(_) /_/ /_  ____  / /_
+        | | /| / / __ `/ / __/ __ \/ __ \/ __/
+        | |/ |/ / /_/ / / /_/ /_/ / /_/ / /_  
+        |__/|__/\__,_/_/\__/_.___/\____/\__/  
+                                    
         =================================================
         """
 
-        self.welcome_msg = 'Hello, I am a tiago waiter.\nWhat would you like to order?'
+        self.welcome_msg = 'Hello, I am a Waitbot.\nWhat would you like to order?'
 
-        rospy.init_node('request_order_node')
-        self.order_pub = rospy.Publisher('/waitbot/orderList', String, queue_size=5)
+        # rospy.init_node('request_order_node')
+        self.order_pub = rospy.Publisher(
+            '/waitbot/orderList', String, queue_size=5)
 
-    def typewriter(self,msg):
+    def typewriter(self, msg):
         for char in msg:
             sleep(0.1)
             sys.stdout.write(char)
             sys.stdout.flush()
 
-    def print_items(self,items):
+    def print_items(self, items):
         msg = items[0]
         for i in range(1, len(items)):
-            print (msg, end="\r")
+            print (msg+"\r")
             sleep(0.3)
             msg += ' ' + items[i]
         print('')
 
     def wrong_order(self):
-        os.system('clear')
-        print(self.banner)
-
-        self.typewriter('Please order again')
-        print('\n') 
+        print('\n')
 
         order = []
 
         while len(order) < 3:
-            order_str = input('Order item: ').strip().lower()
+            order_str = raw_input('Order item: ').strip().lower()
             if rospy.is_shutdown():
                 return False
 
@@ -82,18 +79,19 @@ class req_order():
 
         if rospy.is_shutdown():
             return False
-        
+
         print('\nYou ordered: ' + ', '.join(order))
-        valid = input('If your order is correct press OK or press anything to order again:\n')
+        valid = raw_input(
+            'If your order is correct press OK or press anything to order again:\n')
         if valid == 'OK':
             self.order_pub.publish(json.dumps(order))
             return True
-        else: 
+        else:
             print('Please order again')
             return "loop"
 
-    def main(self):
-        os.system('clear')
+    def order(self):
+        print('\n')
         print(self.banner)
 
         self.typewriter(self.welcome_msg)
@@ -105,7 +103,7 @@ class req_order():
         order = []
 
         while len(order) < 3:
-            order_str = input('Order item: ').strip().lower()
+            order_str = raw_input('Order item: ')  # .strip().lower()
             if rospy.is_shutdown():
                 return False
 
@@ -117,33 +115,32 @@ class req_order():
 
         if rospy.is_shutdown():
             return False
-        
+
         print('\nYou ordered: ' + ', '.join(order))
-        valid = input('If your order is correct press OK or press anything to order again:\n')
+        valid = raw_input(
+            'If your order is correct press OK or press anything to order again:\n')
         if valid == 'OK':
             self.order_pub.publish(json.dumps(order))
             return True
-        else: 
+        else:
             print('Please order again')
             return "loop"
 
+    def main(self):
+        answer = self.order()
+
+        while True:
+            try:
+                if answer is 'loop':
+                    pass
+                elif answer is False or answer is True:
+                    return True
+
+            except rospy.ROSInterruptException:
+                rospy.loginfo("Order is disrupted")
+
+            answer = self.wrong_order()
+
 
 if __name__ == '__main__':
-
     main = req_order()
-    answer = main.main()
-
-    while True:
-        try:
-            if answer is 'loop':
-                pass
-            elif answer is False or answer is True:
-                break
-        
-        except rospy.ROSInterruptException:
-            rospy.loginfo("Order is complete")
-
-        answer = main.wrong_order()
-    # rospy.spin()
-
-
