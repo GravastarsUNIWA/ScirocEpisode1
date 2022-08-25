35b3bc3ab1eb3a5d65c7cb0c56934de18639ffda
Paris Chatzithanos <parischatz94@gmail.com>
2022-08-25 14:43:25 +0300

    Yolo weights fix

---
+++ ros_yolov5/weights/download_weights.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# use following link to download weights for people detection
+# https://drive.google.com/file/d/1gglIwqxaH2iTvy6lZlXuAcMpd_U0GCUb/view?usp=sharing
+
+# use following link to download weights
+# https://github.com/ultralytics/yolov5/releases/download/v6.2/yolov5l.pt
+# https://github.com/ultralytics/yolov5/releases/download/v6.2/yolov5m.pt
+# https://github.com/ultralytics/yolov5/releases/download/v6.2/yolov5n.pt
+# https://github.com/ultralytics/yolov5/releases/download/v6.2/yolov5s.pt
